3b891a7606b8514baf26152b02306fe742b0f729
kdbates31-arch <kdbates31@gmail.com>
2026-05-23 15:30:43 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://awavzlpefkoejptgxias.supabase.co/rest/v1/";
-const supabaseKey = "sb_publishable_5cOwxEU0h7JfchVi_gIEmg_GbnOVlc0Y";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
+const supabaseKey = "sb_publishable_5cOwxEU0h7JfchVi_gIEmg_GbnOVlc0";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
